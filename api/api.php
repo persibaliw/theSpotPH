a0eeaa1d7064d16ff07babc88fbf950657eb55e0
persibaliw <johnpercivalaguilar01@gmail.com>
2026-05-16 09:12:33 +0800

    Update api.php

--- api/api.php
+++ api/api.php
@@ -42,7 +42,6 @@ try {
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         @PDO::MYSQL_ATTR_SSL_CA => $ssl_ca,
         @PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4",
     ];
     $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (PDOException $e) {
